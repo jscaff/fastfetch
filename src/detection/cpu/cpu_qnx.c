1583472340e831c80f5366f47312aee53e52b95f
jscaff <jscaffidi@qnx.com>
2026-09-21 22:44:46 +0000

    first go at a qnx cpu information implementation

--- src/detection/cpu/cpu_qnx.c
+++ src/detection/cpu/cpu_qnx.c
@@ -2,10 +2,19 @@
 #include <sys/syspage.h>
 
 const char* ffDetectCPUImpl(const FFCPUOptions* options, FFCPUResult* cpu) {
-    // struct cpuinfo_entry *cpuinfo = _SYSPAGE_ENTRY(_syspage_ptr, cpuinfo);
-    // cpu->name = SYSPAGE_ENTRY(strings)->data + cpuinfo->name;
-    // cpu->coresPhysical = _syspage_ptr->num_cpu;
-    // cpu->coresLogical = 0;
+    struct cpuinfo_entry *cpuinfo = SYSPAGE_ENTRY(cpuinfo);
+    ffStrbufAppendS(&cpu->name, SYSPAGE_ENTRY(strings)->data + cpuinfo->name);
 
-    return "Not supported on this platform";
+    // Logical cores aren't real and QNX doesn't support hyperthreading
+    cpu->coresOnline = cpu->coresLogical = cpu->coresPhysical = _syspage_ptr->num_cpu;
+
+    // TODO implement aarch64 detection using cpuinfo->flags
+    ffCPUDetectByCpuid(cpu);
+
+    // Naive implementation assuming symmetric cores
+    // A more granular breakdown can be implemented in the future
+    cpu->frequencyBase = cpu->frequencyMax = cpu->coreTypes[0].freq = cpuinfo->speed;
+    cpu->coreTypes[0].count = _syspage_ptr->num_cpu;
+
+    return nullptr;
 }
---
+++ src/detection/cpucache/cpucache_qnx.c
@@ -0,0 +1,33 @@
+#include "cpucache.h"
+#include <stdio.h>
+#include <sys/syspage.h>
+
+void recurseGetCache(FFCPUCacheResult* result, uint32_t idx, uint32_t prevLevel) {
+    if (idx == CACHE_LIST_END) return;
+    struct cacheattr_entry *cacheattr = SYSPAGE_ENTRY(cacheattr);
+
+    FFCPUCacheType cacheType = FF_CPU_CACHE_TYPE_UNIFIED;
+    if (cacheattr->flags & CACHE_FLAG_INSTR) {
+        cacheType = FF_CPU_CACHE_TYPE_INSTRUCTION;
+    } else if (cacheattr->flags & CACHE_FLAG_DATA) {
+        cacheType = FF_CPU_CACHE_TYPE_DATA;
+    } else if (cacheattr->flags & CACHE_FLAG_UNIFIED) {
+        cacheType = FF_CPU_CACHE_TYPE_UNIFIED;
+    }
+
+    ffCPUCacheAddItem(result, prevLevel + 1, cacheattr->line_size * cacheattr->num_lines, cacheattr->line_size, cacheType);
+
+    recurseGetCache(result, cacheattr->next, prevLevel + 1);
+}
+
+const char* ffDetectCPUCache(FFCPUCacheResult* result) {
+    struct cpuinfo_entry *cpuinfo = SYSPAGE_ENTRY(cpuinfo);
+
+    for (int i=0; i < _syspage_ptr->num_cpu; i++) {
+        recurseGetCache(result, cpuinfo[i].ins_cache, 0);
+        recurseGetCache(result, cpuinfo[i].data_cache, 0);
+    }
+
+    return nullptr;
+}
+
