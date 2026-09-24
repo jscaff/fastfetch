e14496c05cc09619cd24c8f9f485c044e634c3fc
jscaff <jscaffidi@qnx.com>
2026-09-24 19:17:49 +0000

    processes options

--- src/detection/processes/processes_qnx.c
+++ src/detection/processes/processes_qnx.c
@@ -16,10 +16,14 @@ const char* ffDetectProcesses(const FFProcessesOptions* options, FFProcessesResu
         while ((entry = readdir(dfd)) != nullptr) {
             // Only match integer entries since they're the pids
             if (sscanf(entry->d_name, "%d", &pid) <= 0) continue;
+            if (!options->countKprocs && pid == 1) { // Kernel Task
+                continue;
+            }
 
             snprintf(buf, sizeof(buf), "/proc/%d/ctl", pid);
             int ctlfd = open(buf, O_RDONLY);
             if (ctlfd == -1) continue;
+
 
             procfs_info info;
             if (devctl(ctlfd, DCMD_PROC_INFO, &info, sizeof(info), nullptr) == EOK) {
