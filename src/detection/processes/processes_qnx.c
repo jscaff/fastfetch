373169430a77df880e0ba0811d8a9c8111130920
jscaff <jscaffidi@qnx.com>
2026-09-24 13:40:56 +0000

    qnx process count

---
+++ src/detection/processes/processes_qnx.c
@@ -0,0 +1,36 @@
+#include "processes.h"
+#include <sys/debug.h>
+#include <sys/procfs.h>
+#include <devctl.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+
+const char* ffDetectProcesses(const FFProcessesOptions* options, FFProcessesResult* result) {
+    DIR* dfd = opendir("/proc");
+    if (dfd) {
+        struct dirent* entry;
+        char buf[PATH_MAX] = {0};
+        int pid = 0;
+        while ((entry = readdir(dfd)) != nullptr) {
+            // Only match integer entries since they're the pids
+            if (sscanf(entry->d_name, "%d", &pid) <= 0) continue;
+
+            snprintf(buf, sizeof(buf), "/proc/%d/ctl", pid);
+            int ctlfd = open(buf, O_RDONLY);
+            if (ctlfd == -1) continue;
+
+            procfs_info info;
+            if (devctl(ctlfd, DCMD_PROC_INFO, &info, sizeof(info), nullptr) == EOK) {
+                result->processes++;
+                result->threads += info.num_threads;
+            }
+
+            close(ctlfd);
+        }
+    }
+    closedir(dfd);
+
+    return nullptr;
+}
